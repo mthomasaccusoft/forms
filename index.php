7329af79d9a3515691b106e34bd5c5f3bef635ea
Mijul Thomas <mthomas@accusoft.com>
2016-11-17 16:30:42 -0500

    Hubspot form snippet added

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-Welcome!
+<h1>mthomas hsform</h1>
+
+
+<!--[if lte IE 8]>
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+<![endif]-->
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+<script>
+  hbspt.forms.create({ 
+    portalId: '106921',
+    formId: '8df4f15f-2f88-448f-be8c-30d5fecf53c5'
+  });
+</script>
+
